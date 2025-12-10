3354f47e7318d8ae2d1023ceeaf19e2c52e6cb88
Talal Nadeem <talalnad10@192.168.1.7>
2025-12-10 19:59:57 +0500

    polished frontend for gestures

--- frontend/app/auth/signup.js
+++ frontend/app/auth/signup.js
@@ -16,7 +16,7 @@ import { useRouter } from "expo-router";
 import colors from "../../src/constants/colors";
 import QuestionnaireSection from "../../src/components/QuestionnaireSection";
 
-const API_BASE_URL = "http://192.168.1.4:8000"; // 👈 your Mac IP
+const API_BASE_URL = "http://192.168.1.7:8000"; // 👈 your Mac IP
 
 export default function SignupScreen() {
   const router = useRouter();
